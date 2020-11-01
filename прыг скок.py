fd4a09bf1e16dbe5ca4a60fac4b302cfc6086c79
fenixdenis <66134026+fenixdenis@users.noreply.github.com>
2020-11-01 16:46:12 +0300

    Change1

--- прыг скок.py
+++ прыг скок.py
@@ -6,7 +6,6 @@ tk = Tk()
 tk.title("Игра")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=400, bd=0,
-                higligthtthickness=0)
+canvas = Canvas(tk, width=500, height=400, bd=0)
 canvas.pack()
 tk.update()
